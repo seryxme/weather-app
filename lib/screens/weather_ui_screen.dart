4c80edd3d30f8c90821281de35d6389f49070c96
seryxme <coolbams24@yahoo.com>
2023-01-24 16:58:42 +0100

    updates

---
+++ lib/screens/weather_ui_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherUIScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class _WeatherUIScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
